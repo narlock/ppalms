ab45551b282a79c2f36b43a143403a9ab1be8a54
narlock <anthonynarlock@outlook.com>
2022-11-16 08:50:28 -0600

    Update PpalmsGui.java

--- PPALMS/src/view/PpalmsGui.java
+++ PPALMS/src/view/PpalmsGui.java
@@ -2,8 +2,14 @@ package view;
 
 import javax.swing.JFrame;
 
+import controller.PplamsInputHandler;
+
 public class PpalmsGui extends JFrame {
 	private ViewStrategy viewStrategy;
+	private PplamsInputHandler controller;
 	
+	public void setCommunicationActions() {
+		//TODO
+	}
 	
 }

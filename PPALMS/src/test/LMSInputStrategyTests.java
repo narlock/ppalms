1f6037f70170d4a68ff9d466119625126e2e9f98
narlock <anthonynarlock@outlook.com>
2022-11-16 17:04:25 -0600

    updated tests and set communicaion actions

---
+++ PPALMS/src/test/LMSInputStrategyTests.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LMSInputStrategyTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

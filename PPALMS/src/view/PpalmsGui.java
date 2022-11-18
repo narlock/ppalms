7538edd29acfc1244b188a689e13a2dab50a5bea
Anthony <59890724+narlock@users.noreply.github.com>
2022-11-18 09:06:31 -0600

    Create UI elements for ProbleInputStrategy

--- PPALMS/src/view/PpalmsGui.java
+++ PPALMS/src/view/PpalmsGui.java
@@ -28,7 +28,8 @@ public class PpalmsGui extends JFrame {
 		this.setTitle("PPALMS v0.0.1");
 		
 		// The default view strategy on launch will be CodeInputStrategy
-		this.viewStrategy = new CodeInputStrategy();
+//		this.viewStrategy = new CodeInputStrategy();
+		this.viewStrategy = new ProblemInputStrategy();
 		this.add(viewStrategy);
 		
 		this.controller = new PpalmsInputHandler();
--- PPALMS/src/view/ProblemInputStrategy.java
+++ PPALMS/src/view/ProblemInputStrategy.java
@@ -1,5 +1,9 @@
 package view;
 
+import java.awt.Dimension;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,7 +15,7 @@ public class ProblemInputStrategy extends ViewStrategy {
 	private JLabel titleInputLabel;
 	private JTextField titleInputTextField;
 	private JLabel descriptionInputLabel;
-	private JTextField descriptionInputTextField;
+	private JTextArea descriptionInputTextField;
 	private JLabel annotationsLabel;
 	private JTextArea sourceCodeArea; //May be changed... May need something better for ordering.. maybe stacked panels that are movable.
 	// private List<JPanel> sourceCodeArea; // We can use this to stack problem lines on top of one another
@@ -20,19 +24,37 @@ public class ProblemInputStrategy extends ViewStrategy {
 	@Override
 	public void setViewPanel() {
 		// TODO Auto-generated method stub
+		JPanel verticalPanel = new JPanel();
+		verticalPanel.setLayout(new BoxLayout(verticalPanel, BoxLayout.Y_AXIS));
+		titleInputLabel = new JLabel("Problem Title");
+		titleInputTextField = new JTextField(10);
+		descriptionInputLabel = new JLabel("Problem Description");
+		descriptionInputTextField = new JTextArea(5, 10);
+		annotationsLabel = new JLabel("Annotations");
+		exportProblem = new JButton("Export Problem");
 		
+		verticalPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+		verticalPanel.add(titleInputLabel);
+		verticalPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+		verticalPanel.add(titleInputTextField);
+		verticalPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+		verticalPanel.add(descriptionInputLabel);
+		verticalPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+		verticalPanel.add(descriptionInputTextField);
+		verticalPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+		verticalPanel.add(annotationsLabel);
+		verticalPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+		verticalPanel.add(exportProblem);
+		this.add(verticalPanel);
 	}
 
 	@Override
 	public void setControllerActions() {
-		// TODO Auto-generated method stub
-		
+		exportProblem.setEnabled(false);
 	}
 
-	@Override
-	public void showErrorDialog(String message) {
-		// TODO Auto-generated method stub
-		
-	}
+	public JTextField getTitleInputTextField() { return titleInputTextField; }
+	public JTextArea getDescriptionInputTextField() { return descriptionInputTextField; }
+	public JButton getExportProblem() { return exportProblem; }
 
 }

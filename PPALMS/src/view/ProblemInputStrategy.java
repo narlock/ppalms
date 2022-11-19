75cf262d801880b2cfd070974d3155b9c20a6518
Anthony <59890724+narlock@users.noreply.github.com>
2022-11-18 22:33:49 -0600

    Added javadoc for everything

--- PPALMS/src/view/ProblemInputStrategy.java
+++ PPALMS/src/view/ProblemInputStrategy.java
@@ -1,10 +1,7 @@
 package view;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -12,22 +9,60 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JScrollBar;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+/**
+ * ProblemInputStrategy is the view "state" in which
+ * the PPALMS application will allow the user to input
+ * problem title, problem description, create
+ * problem annotations, and export the problem.
+ * 
+ * @author narlock
+ *
+ */
 public class ProblemInputStrategy extends ViewStrategy {
 
+	/**
+	 * The label component indicating title for problem.
+	 */
 	private JLabel titleInputLabel;
+	
+	/**
+	 * The text field component that the user can input
+	 * title with their keyboard for the problem.
+	 */
 	private JTextField titleInputTextField;
+	
+	/**
+	 * The label component indicating description
+	 * for the problem.
+	 */
 	private JLabel descriptionInputLabel;
+	
+	/**
+	 * The text area component that the user can input
+	 * description with their keyboard for the problem.
+	 */
 	private JTextArea descriptionInputTextField;
+	
+	/**
+	 * The label component indicating annotation
+	 * area for the problem.
+	 */
 	private JLabel annotationsLabel;
-	private JPanel contentPane;
+	
+	/**
+	 * The sourceCodeArea and annotationPanel are
+	 * panels that define problem annotation interface.
+	 */
+	private JPanel sourceCodeArea;
 	private JPanel annotationPanel;
-	private JScrollPane annotationPane;
-	private JTextArea sourceCodeArea; //May be changed... May need something better for ordering.. maybe stacked panels that are movable.
-	// private List<JPanel> sourceCodeArea; // We can use this to stack problem lines on top of one another
+	
+	/**
+	 * The button component that allows the user
+	 * to export their created problem.
+	 */
 	private JButton exportProblem;
 	
 	@Override
@@ -48,9 +83,9 @@ public class ProblemInputStrategy extends ViewStrategy {
 	        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 	        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 	        scrollPane.setSize(450,200);
-        contentPane = new JPanel(null);
-        contentPane.setPreferredSize(new Dimension(500, 400));
-        contentPane.add(scrollPane);
+        sourceCodeArea = new JPanel(null);
+        sourceCodeArea.setPreferredSize(new Dimension(500, 400));
+        sourceCodeArea.add(scrollPane);
 		exportProblem = new JButton("Export Problem");
 		
 		verticalPanel.add(Box.createRigidArea(new Dimension(0, 10)));
@@ -64,14 +99,14 @@ public class ProblemInputStrategy extends ViewStrategy {
 		verticalPanel.add(Box.createRigidArea(new Dimension(0, 10)));
 		verticalPanel.add(annotationsLabel);
 		verticalPanel.add(Box.createRigidArea(new Dimension(0, 10)));
-		verticalPanel.add(contentPane);
+		verticalPanel.add(sourceCodeArea);
 		this.add(exportProblem, BorderLayout.SOUTH);
 		this.add(verticalPanel);
 	}
 
 	@Override
 	public void setControllerActions() {
-		exportProblem.setEnabled(false); //TODO May need to be set to false depending on problem type.
+		exportProblem.setEnabled(false);
 	}
 
 	public JTextField getTitleInputTextField() { return titleInputTextField; }

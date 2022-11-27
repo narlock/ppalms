598f0dd973ef9e6c2e0ce33588c79063f188264e
Anthony <59890724+narlock@users.noreply.github.com>
2022-11-27 06:38:37 -0600

    Update CodeInputStrategyTests.java

--- PPALMS/src/test/CodeInputStrategyTests.java
+++ PPALMS/src/test/CodeInputStrategyTests.java
@@ -9,12 +9,35 @@ import view.PpalmsGui;
 
 class CodeInputStrategyTests {
 
+	/**
+	 * testCodeInputStrategyAttributesNotNull
+	 * will ensure that the components of the
+	 * CodeInputStrategy is not null.
+	 * 
+	 * @author narlock
+	 * 
+	 */
 	@Test
 	void testCodeInputStrategyAttributesNotNull() {
 		CodeInputStrategy strategy = new CodeInputStrategy();
 		assertNotNull(strategy.getCodeInputButton());
 		assertNotNull(strategy.getCodeInputLabel());
 		assertNotNull(strategy.getFileChooser());
+	}
+	
+	/**
+	 * testCodeInputStrategyAttributesNotNull
+	 * will ensure that the component attributes
+	 * are visually set correctly.
+	 * 
+	 * @author narlock
+	 * 
+	 */
+	@Test
+	void testCodeInputStrategyComponentAttributes() {
+		CodeInputStrategy strategy = new CodeInputStrategy();
+		assertEquals("Input Source Code", strategy.getCodeInputLabel().getText());
+		assertEquals("Select File", strategy.getCodeInputButton().getText());
 	}
 
 }

f0c0516f88c84737724daed2b167b32c3b849106
Shen Lua <shenlua1@gmail.com>
2022-11-28 14:16:43 -0600

    PpalmsGui_tests_and_javadocs_getters

--- PPALMS/src/test/PpalmsGuiTests.java
+++ PPALMS/src/test/PpalmsGuiTests.java
@@ -3,10 +3,16 @@ package test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -23,7 +29,7 @@ import view.PpalmsGui;
 /**
  * PpalmsGuiTests
  * 
- * This class holds all the unite tests 
+ * This class holds all the unit tests 
  * for the PpalmsGuiTests class. 
  * 
  * The following unit tests are specified
@@ -37,7 +43,7 @@ import view.PpalmsGui;
 class PpalmsGuiTests {
 	
 	/**
-	 * testUpdateViewStrategy
+	 * testUpdateViewStrategySuccess
 	 * 
 	 * @brief Tests whether if the view strategy has changed
 	 * from the default "CodeInputStrategy" to "LMSInputStrategy"
@@ -47,11 +53,66 @@ class PpalmsGuiTests {
 	 * @author Shen Lua
 	 */
 	@Test
-	void testUpdateViewStrategy() {
-	  PpalmsGui p = new PpalmsGui();
-	  LMSInputStrategy l = new LMSInputStrategy();
-	  p.updateViewStrategy(l);
-	  assertEquals(true, p.getViewStrategy() instanceof LMSInputStrategy);
+	void testUpdateViewStrategySuccess() {
+		 PpalmsGui gui = new PpalmsGui();
+		 // To prevent errors when transitioning from LMSInputStrategy to ProblemInputStrategy view
+		 List<String> testSourceCodeLines = new ArrayList<String>();
+		 testSourceCodeLines.add("int main() {printf('hello world')}");
+		 gui.getPpalmsInputHandler().getProblem().setSourceCodeLines(testSourceCodeLines);
+		 gui.updateViewStrategy(new LMSInputStrategy());
+		 assertTrue(gui.getViewStrategy() instanceof LMSInputStrategy);
+		 gui.updateViewStrategy(new ProblemInputStrategy());
+		 assertTrue(gui.getViewStrategy() instanceof ProblemInputStrategy);
+	}
+	
+	/**
+	 * testProblemTitleDescriptionInput
+	 * 
+	 * @brief Tests whether both the Title and 
+	 * Description JTextFields are operational and 
+	 * accept user input. 
+	 * 
+	 * @author Shen Lua
+	 */
+	@Test
+	void testProblemTitleDescriptionInput() {
+		PpalmsGui gui = new PpalmsGui();
+		List<String> testSourceCodeLines = new ArrayList<String>();
+		testSourceCodeLines.add("int main() {printf('hello world')}");
+		gui.getPpalmsInputHandler().getProblem().setSourceCodeLines(testSourceCodeLines);
+		gui.updateViewStrategy(new ProblemInputStrategy());
+		JTextField titleInputTextField = ((ProblemInputStrategy) gui.getViewStrategy()).getTitleInputTextField();
+		JTextArea descriptionInputTextField = ((ProblemInputStrategy) gui.getViewStrategy()).getDescriptionInputTextField();
+		titleInputTextField.setText("testTitle");
+		assertTrue(gui.getPpalmsInputHandler().processInput(titleInputTextField,"problemTitle"));
+		descriptionInputTextField.setText("testDescription");
+		assertTrue(gui.getPpalmsInputHandler().processInput(descriptionInputTextField, "problemDescription"));
+	}
+	
+	/**
+	 * testProblemAnnotation
+	 * 
+	 * @brief Tests whether if clicking on 
+	 * a line button for annotation would 
+	 * result in the exportProblem button
+	 * to be enabled. 
+	 * 
+	 * 
+	 * @author Shen Lua
+	 */
+	@Test
+	void testProblemAnnotation() {
+		PpalmsGui gui = new PpalmsGui();
+		List<String> testSourceCodeLines = new ArrayList<String>();
+		testSourceCodeLines.add("int main() {printf('hello world')}");
+		gui.getPpalmsInputHandler().getProblem().setSourceCodeLines(testSourceCodeLines);
+		gui.updateViewStrategy(new ProblemInputStrategy());
+		JButton exportProblem = ((ProblemInputStrategy) gui.getViewStrategy()).getExportProblem();
+		List<String> alines = gui.getPpalmsInputHandler().getPpalmsProblem().getSourceCodeLines();
+		JButton lineButton = gui.getAnnotationLineButton(exportProblem, 0, alines.get(0));
+		lineButton.doClick();
+		lineButton.doClick(); 
+		assertTrue(exportProblem.isEnabled()); 
 	}
 	
 	@Test
@@ -106,16 +167,16 @@ class PpalmsGuiTests {
 	 * @author Shen Lua 
 	 */
 	@Test
-	void testProblemTypeNotSelected() { // no problem type selected
+	void testProblemTypeSelection() {
 		PpalmsGui p = new PpalmsGui();
 		LMSInputStrategy l = new LMSInputStrategy();
 		p.updateViewStrategy(l);
 		JButton confirmLmsTargetButton = ((LMSInputStrategy) p.getViewStrategy()).getConfirmLmsTargetButton();
 		JComboBox<String> problemTypeComboBox = ((LMSInputStrategy) p.getViewStrategy()).getProblemTypeComboBox();
 		problemTypeComboBox.setSelectedIndex(0);
-		assertFalse(confirmLmsTargetButton.isEnabled());
+		assertFalse(confirmLmsTargetButton.isEnabled()); // disabled when none selected
 		problemTypeComboBox.setSelectedIndex(1); // ordering
-		assertTrue(confirmLmsTargetButton.isEnabled());
+		assertTrue(confirmLmsTargetButton.isEnabled()); // enabled when selected
 	}
 	
 }

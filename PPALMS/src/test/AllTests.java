4dad394b460f4c211269390825a3a10b25402c67
Anthony <59890724+narlock@users.noreply.github.com>
2022-11-27 20:32:06 -0600

    updating files for e-e test

--- PPALMS/src/test/AllTests.java
+++ PPALMS/src/test/AllTests.java
@@ -4,7 +4,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({ CodeInputStrategyTests.class, LMSInputStrategyTests.class, PpalmsLogicHandlerTests.class })
+@SelectClasses({ CodeInputStrategyTests.class, LMSInputStrategyTests.class, PpalmsLogicHandlerTests.class, PpalmsGuiTests.class, PpalmsInputHandlerTests.class })
 public class AllTests {
 
 }
---
+++ PPALMS/src/test/example_source.py
@@ -0,0 +1,6 @@
+def main():
+    # The following line should print hello world
+    print('Hello World')
+
+if __name__ == '__main__':
+    main()

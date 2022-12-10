ae1e85a41d72f25a81a1952d5fe42a0e10148067
Anthony <59890724+narlock@users.noreply.github.com>
2022-12-10 14:21:53 -0600

    Update PpalmsProblem.java

--- PPALMS/src/model/PpalmsProblem.java
+++ PPALMS/src/model/PpalmsProblem.java
@@ -225,7 +225,7 @@ public class PpalmsProblem {
 	public void addItemToList(int index) {
 		blankBounds.put(index, new ArrayList<Integer>());
 		blankBounds.get(index).add(0, -1);
-		blankBounds.get(index).add(0, -2);
+		blankBounds.get(index).add(1, -2);
 		System.out.println("blankBounds Size " + blankBounds.size() + ", lower = " + blankBounds.get(index).get(0) + ", upper = " + blankBounds.get(index).get(1));
 	}
 	
